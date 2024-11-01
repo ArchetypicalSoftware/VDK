12dbadf06214d6f73acb3c0281b3c28e7fab67ab
Eddie Wassef <eddie@wassef.com>
2024-11-01 08:59:15 -0500

    refactoring and cleaning up

--- cli/src/Vdk/Services/ReverseProxyClient.cs
+++ cli/src/Vdk/Services/ReverseProxyClient.cs
@@ -66,47 +66,9 @@ internal class ReverseProxyClient : IReverseProxyClient
         // create a new server block in the nginx conf pointing to the target port listening on the https://clusterName.dev-k8s.cloud domain
         // reload the nginx configuration
 
-        var conf = new FileInfo(NginxConf);
+        
 
-        // check all the server blocks and remove the one that matches the clusterName
-        var lines = File.ReadAllLines(conf.FullName).ToList();
-        // chunk all the lines into sections in a dictionary of string, list<string>, where the key is the cluster name and the value is the list of lines betweeh the START and END comments
-        var sections = new Dictionary<string, List<string>>();
-        var currentSection = new List<string>();
-        var currentCluster = string.Empty;
-        foreach (var line in lines)
-        {
-            if (line.Contains(StartComment))
-            {
-                if (currentSection.Any())
-                {
-                    sections.Add(currentCluster, currentSection);
-                    currentSection = new List<string>();
-                }
-                currentCluster = line.Replace(StartComment, string.Empty).Trim();
-            }
-            currentSection.Add(line);
-        }
-        if (currentSection.Any())
-        {
-            sections.Add(currentCluster, currentSection);
-        }
-        if (sections.ContainsKey(clusterName))
-        {
-            sections.Remove(clusterName);
-        }
-
-        // clear the file and write the sections back
-        using var writer = conf.CreateText();
-        foreach (var section in sections)
-        {
-            writer.WriteLine();
-            foreach (var line in section.Value)
-            {
-                writer.WriteLine(line);
-            }
-            writer.WriteLine();
-        }
+        using var writer = ClearCluster(clusterName);
 
         writer.WriteLine();
         writer.WriteLine($"##### START {clusterName}");
@@ -146,12 +108,73 @@ internal class ReverseProxyClient : IReverseProxyClient
         };
         _client(clusterName).Create(tls);
 
+        ReloadConfigs();
+    }
+
+    private void ReloadConfigs()
+    {
         _docker.Exec("nginx", new[] { "nginx", "-s", "reload" });
+    }
+
+    private static StreamWriter ClearCluster(string clusterName)
+    {
+        var conf = new FileInfo(NginxConf);
+        StreamWriter? writer = null;
+        try
+        {
+            // check all the server blocks and remove the one that matches the clusterName
+            var lines = File.ReadAllLines(conf.FullName).ToList();
+            // chunk all the lines into sections in a dictionary of string, list<string>, where the key is the cluster name and the value is the list of lines betweeh the START and END comments
+            var sections = new Dictionary<string, List<string>>();
+            var currentSection = new List<string>();
+            var currentCluster = string.Empty;
+            foreach (var line in lines)
+            {
+                if (line.Contains(StartComment))
+                {
+                    if (currentSection.Any())
+                    {
+                        sections.Add(currentCluster, currentSection);
+                        currentSection = new List<string>();
+                    }
+                    currentCluster = line.Replace(StartComment, string.Empty).Trim();
+                }
+                currentSection.Add(line);
+            }
+            if (currentSection.Any())
+            {
+                sections.Add(currentCluster, currentSection);
+            }
+            if (sections.ContainsKey(clusterName))
+            {
+                sections.Remove(clusterName);
+            }
+
+            // clear the file and write the sections back
+            writer = conf.CreateText();
+            foreach (var section in sections)
+            {
+                writer.WriteLine();
+                foreach (var line in section.Value)
+                {
+                    writer.WriteLine(line);
+                }
+                writer.WriteLine();
+            }
+
+            return writer;
+        }
+        catch
+        {
+            writer?.Dispose();
+            throw;
+        }
     }
 
     public void Delete(string clusterName)
     {
-        throw new NotImplementedException();
+        ClearCluster(clusterName).Flush();
+        ReloadConfigs();
     }
 
     public void List()
